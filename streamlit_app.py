ce8e4e895f03f285c725b39d2ea20c8c6d28979a
Makenna Deason <84406559+makdea@users.noreply.github.com>
2023-05-02 11:38:20 -0500

    Second text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the normalized data
 streamlit.dataframe(fruityvice_normalized)
 
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
